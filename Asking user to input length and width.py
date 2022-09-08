616396fe3f7e486c15e677df6e1779e9079cb638
DoNotDistrub <110594137+DoNotDistrub@users.noreply.github.com>
2022-09-08 21:44:27 +0500

    Initial commit

---
+++ Asking user to input length and width.py
@@ -0,0 +1,10 @@
+L = float(input("Please tell us your lenght : "))
+W = float(input("Please tell us your width : "))
+
+print("The area")
+
+print(L * W)
+
+print("The perimeter")
+
+print((L + W) *2 )
